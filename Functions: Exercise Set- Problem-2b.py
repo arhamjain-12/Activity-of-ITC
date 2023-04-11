9e9eb5479c2c87ec6ca0a3fb886e8c855f4d7373
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 15:53:47 +0000

    main

--- Functions: Exercise Set- Problem-2b.py
+++ Functions: Exercise Set- Problem-2b.py
@@ -1,1 +1,17 @@
 
+
+"""
+(b) How one can check if the restriction is on a number of words rather than letters?
+Write a function that allows a message with only 30 words.
+"""
+def chit_word(message):
+    word=message.split()
+    if len(word) <=30:
+        return message
+    else:
+        return 'message is greater then 30 words'
+        
+        
+b=input()
+a=chit_word(b)
+print(a)
