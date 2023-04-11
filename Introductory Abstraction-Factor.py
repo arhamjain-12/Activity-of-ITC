3937e65ac653efca4adff6f9bf1b1d59654e5f97
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 15:58:39 +0000

    main

--- Introductory Abstraction-Factor.py
+++ Introductory Abstraction-Factor.py
@@ -1,1 +1,9 @@
 
+"""
+Factor:  Calculate temperature that a person feels because of the wind.
+"""
+def factor(A,B):
+    C = 13.12 + 0.6215 * A * (0.3965 * A - 11.37) * (B**0.16)
+    return C
+factor(1,2)
+Footer
