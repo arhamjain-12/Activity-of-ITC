f2ffb34af780a68e3b5ef3b66e310d7be3859473
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 15:53:01 +0000

    main

--- Functions: Exercise Set- Problem-2a.py
+++ Functions: Exercise Set- Problem-2a.py
@@ -1,15 +1,17 @@
 """
-In few chit-chat programs, there is a limitation on the number of letters that you can send to your family and friends.
+Chit Chat Application - Function:
+(a) In few chit-chat programs, there is a limitation on the number of letters that you can send to your family and friends.
 Write a function that takes as input the,
 message and checks whether the number of letters is less than 200 or above. If the length of the information is less than 200, the chat should be returned.
 If the length of the chat is greater than 200, data consisting of only the first 200 characters should be returned.
 """
-
-a=input()
-def chat(a):
-    if len(a)<=200:
-        return a
+def chit_chat(message):
+    if len(message) <=200:
+        return message
     else:
-        b=a[:200]
-        return b
-print(chat(a))
+        return 'message is greater then 200 letters'
+
+
+b=input()
+a=chit_chat(b)
+print(a)
