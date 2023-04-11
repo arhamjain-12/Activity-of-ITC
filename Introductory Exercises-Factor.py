f10c7f22b5b167ec41f7e2c588f975b9230dbed1
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 16:04:44 +0000

    main

--- Introductory Exercises-Factor.py
+++ Introductory Exercises-Factor.py
@@ -1,1 +1,8 @@
 
+"""
+Factor:  Calculate temperature that a person feels because of the wind (Python3)
+"""
+A=float(input('Input air temperature in degree celsius: '))
+B=float(input('Input wind speed in km/h: '))
+C = 13.12 + 0.6215 * A * (0.3965 * A - 11.37) * (B**0.16)
+print(C)
