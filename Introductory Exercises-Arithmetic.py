0e253396ccb81b4db0928c20a0c4e5723cd9acd1
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 16:02:06 +0000

    main

--- Introductory Exercises-Arithmetic.py
+++ Introductory Exercises-Arithmetic.py
@@ -1,1 +1,24 @@
 
+"""
+Arithmetic: Input some numbers, do some simple arithmetic, output results (Python3)
+"""
+
+n=int(input())
+a=int(input())
+b=int(input())
+if n==1:
+    print(a+b)
+elif n==2:
+    print(a-b)
+elif n==3:
+    print(a*b)
+elif n==4:
+    print(a/b)
+elif n==5:
+    print(a%b)
+elif n==6:
+    print(a//b)
+elif n==7:
+    print(a**b)
+else:
+    print('Invalid Input')
