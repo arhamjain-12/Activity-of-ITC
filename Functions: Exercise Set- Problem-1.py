593e4cb9c46dd9b1f85e0d6368d05d65ce965e2a
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 16:10:59 +0000

    mian

--- Functions: Exercise Set- Problem-1.py
+++ Functions: Exercise Set- Problem-1.py
@@ -1,21 +1,16 @@
 
 """
-Suppose you are buying something online on amazon.com
-On the website, you get a 15% discount if you are a prime member. Additionally, you are also getting a discount of 8% on the item because it's black Friday sales.
+Suppose you are buying something online on amazon.com 
+On the website, you get
+a 15% discount if you are a prime member. Additionally, you are also getting a discount of 8% on the item because it's black Friday sales.
 Write a function that takes as input the amount of the asset that you are buying and a logical variable indicating whether you are a prime member applies the discount offered appropriately and returns the result.
 """
-
-a=float(input("Enter Amount: "))
-b=input('Are you a Prime member : ')
-def discount(a,b):
-    if b=='yes' or b=='Yes' or b=='YES':
-        d1=0.15*a
-        c=a-d1
-        bfd=0.08*c
-        f_p=c-bfd
-        return (f_p)
+def discount(amount,prime_member,friday):
+    if prime_member and friday:
+        return (amount-((23*amount)/100))
+    elif prime_member:
+        return (amount-((15*amount)/100))
     else:
-        s=0.08*a
-        r=a-s
-        return(r)
-print(discount(a,b))
+        return 'No discount'
+        
+print(discount(1200,True,False))
