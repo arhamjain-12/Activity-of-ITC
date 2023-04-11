f7497a4a1694fcb5c729967f003375a015220c35
arhamjain-12 <126847567+arhamjain-12@users.noreply.github.com>
2023-04-11 16:05:29 +0000

    main

--- Set in Python.py
+++ Set in Python.py
@@ -1,1 +1,23 @@
 
+"""Problem - I 
+sentence = "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drink"
+print(verse, "\n")
+# split sentence into list of words
+sentence_list =  # You will have to fill out the function 
+print(sentence_list, '\n')
+# convert list to set to get unique words
+sentence_set = 
+print(sentence_set, '\n')
+# print the number of unique words
+num_unique = 
+print(num_unique, '\n')"""
+
+sentence= "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drink"
+A=sentence.split()
+print("Verse= ",*A)
+print("sentence_list= ",A,"\n")
+
+B=set(A)
+print("sentence_set= ",B,"\n")
+
+print("num_unique: ",len(B),"\n")
